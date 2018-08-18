9e2e63b551beb89fc6ca357f3ca2dbec2fdd02d5
Justin Golden <justingolden21@gmail.com>
2018-08-17 20:44:17 -0700

    edit ads
    remove google ads script, ad default ad search for amazon

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function getData(term){
   $results[0].insertAdjacentHTML('beforeend', '<p><i class="fas fa-sync fa-spin"></i><b> Gathering results...</b></p><hr>');
 
   //for amazon ads
-  amzn_assoc_default_search_phrase = term;
+  // amzn_assoc_default_search_phrase = term;
 
   try {
   $.ajax({
